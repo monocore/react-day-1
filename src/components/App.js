83684e9b44cbeb3da411081b850ab0218d92eaef
Mark Scheper <scheper.m@tkppensioen.nl>
2017-06-13 14:43:31 +0200

    theming

--- src/components/App.js
+++ src/components/App.js
@@ -3,35 +3,30 @@ import Form from "react-jsonschema-form";
 import Widgets from "./Widgets";
 
 const schema = {
-  title: "Waardeovername",
-  type: "object",
-  required: ["title"],
-  properties: {
-    naam: {
-      type: "string",
-      title: "Naam"
+  "title": "Waardeovername",
+  "type": "object",
+  "required": ["title"],
+  "properties": {
+    "naam": {
+      "type": "string",
+      "title": "Naam"
     },
-    psnId: {
-      type: "string",
-      title: "Pensioennummer",
-    },
-    title: {
-      type: "string",
-      title: "Title",
-      default: "A new task"
-    },
-    done: {
-      type: "boolean",
-      title: "Done?",
-      default: false
+    "psnId": {
+      "type": "integer",
+      "title": "Pensioennummer",
     }
   }
 };
 
 const uiSchema = {
-  naam: {
+  "naam": {
     "ui:widget": "inlineText"
   }
+};
+
+const formData = {
+  "naam": "I.S. Boneva-Rensink",
+  "psnId": 6205911
 };
 
 const log = (type) => console.log.bind(console, type);
@@ -40,11 +35,14 @@ const App = () => (
   <Form
     schema={schema}
     uiSchema={uiSchema}
+    formData={formData}
     widgets={Widgets}
     onChange={log("changed")}
     onSubmit={log("submitted")}
     onError={log("errors")}
-  />
+  >
+    <button type="submit" className="btn btn-primary">Versturen</button>
+  </Form>
 );
 
 export default App;
